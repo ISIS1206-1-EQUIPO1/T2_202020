8498c440154b445827ba939438fe8c97a3b9e079
dfgomezm22 <df.gomezm@uniandes.edu.co>
2020-08-30 14:43:40 -0500

    Finalizacion linked list 30/08/20

--- src/model/data_structures/LinkedList.java
+++ src/model/data_structures/LinkedList.java
@@ -162,17 +162,28 @@ public class LinkedList< T extends Comparable< T > > implements IDataStructure< 
 
 	public T lastElement() throws ElementNotFoundException 
 	{
-		return null;
+	    if(actSize == 0)
+	        throw new ElementNotFoundException("La lista esta vacía");
+	    else if(actSize == 1)
+	       return header.getElement();
+	    else
+	    {
+	        Node <T> actual = header;
+	        while(actual.getNext( ) != null)
+	            actual = actual.getNext( );
+	        return actual;
+	    }
 	}
 
-	public T getElementPos(int i) throws ElementNotFoundException, InvalidIndexException 
+	//REVISEN ESTEEEEE#########
+	public T getElementPos(int i) throws InvalidIndexException 
 	{
-		return null;
-	}
-
-	public T getElement(T dato) throws ElementNotFoundException 
-	{
-		return null;
+	    if(i <  1 || i > actSize)
+	        throw new InvalidIndexException("El indice solicitado no se encuentra disponible la lista actual");
+	    Node <T> actual = header;
+	    for( int j= 1; j<i; j++)
+	    	actual = actual.getNext();
+	    return actual.getElement();
 	}
 
 	public boolean isEmpty() 
@@ -182,17 +193,62 @@ public class LinkedList< T extends Comparable< T > > implements IDataStructure< 
 
 	public int isPresent(T element) throws ElementNotFoundException 
 	{
-		return 0;
+	    int pos = -1;
+	    T found = null;
+	    boolean get = false;
+	    Node <T> actual = header;
+	    for(int i = 1; i < actSize && !get; i++)
+	    {
+	        if(actual.getElement().compareTo(element) == 0)
+	        {
+	            pos = i;
+	            get = true;
+	        }
+	        actual = actual.getNext( );
+	    }
+	    return pos;
 	}
 
 	public void exchange(int pos1, int pos2) throws ElementNotFoundException, InvalidIndexException 
 	{
-
+		if( pos1 < 1 || pos1 > actSize || pos2 < 1 || pos2 > actSize)
+			throw new InvalidIndexException( "El indice debe estar entre 1 y el tamanio del arreglo." );
+		
+		Node<T> nodo1 = null;
+		Node<T> nodo2 = null; 
+		Node <T> actual = header;
+		for (int i = 1; i <= actSize; i++) 
+		{
+			if(i == pos1)
+				nodo1 = actual;
+			if(i==pos2)
+				nodo2 = actual;
+			actual = actual.getNext();
+		}
+		
+		if(nodo1 == null || nodo2 == null)
+		{
+			throw new ElementNotFoundException ( " No existen elementos en las posiciones indicadas " );
+		}
+		else
+		{
+			T element = nodo1.getElement();
+			nodo1.setElement(nodo2.getElement());
+			nodo2.setElement(element);
+		}
+		
 	}
 
 	public void changeInfo(int pos, T elem) throws ElementNotFoundException, InvalidIndexException 
 	{
-
+		if( pos < 1 || pos > actSize)
+			throw new InvalidIndexException( "El indice debe estar entre 1 y el tamanio del arreglo." );
+		if( elem == null )
+			throw new ElementNotFoundException( "El elemento a eliminar no puede ser null" );
+		Node <T> actual = header;
+		for( int j= 1; j<pos; j++)
+	    	actual = actual.getNext();
+	    actual.setElement(elem);
 	}
 
 }
